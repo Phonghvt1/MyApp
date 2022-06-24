299e1a384798afa78975f971b4c782ec1b6995af
Phonghvt1 <108001016+Phonghvt1@users.noreply.github.com>
2022-06-24 21:19:21 +0700

    Buoi 5
    asd

---
+++ SuDungMang/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuDungMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KhaiBaoMang();
+            //XuatMangRaManHinh();
+            //XuatSoChanTrongMang();
+            //TimGiaTriLonNhatTrongMang;
+            NhapMangSoNguyen();
+
+            Console.ReadKey();
+        }
+
+        private static void NhapMangSoNguyen()
+        {
+            int SoPhanTu = NhapSoNguyenDuong("Nhap so phan tu: ");
+            int[] a = new int[SoPhanTu];
+            for (int i = 0; i < SoPhanTu; i++)
+            {
+                a[i] = NhapSoNguyenDuong(string.Format("Nhap Phan Tu Thu {0}", i + 1));
+            }
+        }
+
+        private static int NhapSoNguyenDuong(string thongbao)
+        {
+            int a;
+            bool kt;
+            Console.WriteLine(thongbao);
+            do
+            {
+                kt = int.TryParse(Console.ReadLine(), out a);
+                if (kt == true)
+                {
+                    if (a <= 0)
+                    {
+                        kt = false;
+                    }
+                }
+                if (kt == false)
+                {
+                    Console.WriteLine("Nhap lai: ");
+                }
+            } while (kt == false);
+
+            return a;
+        }
+
+        private static void TimGiaTriLonNhatTrongMang()
+        {
+            int[] a = { 23, 56, 45, 78, 98, 45, 32, 95 };
+            int b = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i]>b)
+                {
+                    b = a[i];
+                }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine("So lon nhat la: {0}", b);
+        }
+
+        private static void XuatSoChanTrongMang()
+        {
+            int[] a = { 23, 56, 45, 78, 98, 45, 32, 95 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    Console.WriteLine("{0} la so chan",a[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0} la so le", a[i]);
+                }
+            }
+        }
+
+        private static void XuatMangRaManHinh()
+        {
+            int[] a = { 23, 56, 45, 78, 98, 45, 32, 95 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        private static void KhaiBaoMang()
+        {
+            int[] a = { 23, 12, 1, 2, 5, 10, 223, 32};
+            int[] b = new int[4];
+
+        }
+    }
+}
---
+++ SuDungVongLapWhileTT/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuDungVongLapWhileTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UCLN();
+            //XacDinhSoNguyenTo();
+            //XacDinhNguyenTo();
+            //int a = NhapSoNguyen();
+            int b = NhapSoNguyenDuong();
+            Console.ReadKey();
+        }
+
+        private static int NhapSoNguyenDuong()
+        {
+            int a;
+            bool kt;
+            Console.WriteLine("Nhap so nguyen duong: ");
+            do
+            {
+                kt = int.TryParse(Console.ReadLine(), out a);
+                if (kt == true)
+                {
+                    if (a <= 0)
+                    {
+                        kt = false;
+                    }
+                }
+                if (kt == false)
+                {
+                    Console.WriteLine("Nhap lai: ");
+                }
+            } while (kt==false);
+            return a;
+        }
+
+        private static int NhapSoNguyen()
+        {
+            int a;
+            Console.WriteLine("Nhap so nguyen a: ");
+            while (int.TryParse(Console.ReadLine(),out a) == false)
+            {
+                Console.WriteLine("Nhap lai: ");
+            }
+            return a;
+        }
+
+        private static void XacDinhSoNguyenTo()
+        {
+            Console.WriteLine("Nhap so nguyen to N: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 2;
+            while (i <= n)
+            {
+                if (n%i == 0)
+                {
+                    if (n == i)
+                    {
+                        Console.WriteLine("{0} la so nguyen to", n);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} khong la so nguyen to", n);
+                    }
+                    break;
+                }
+                i++;
+            }
+        }
+
+        private static void XacDinhNguyenTo()
+        {
+            Console.WriteLine("Nhap so nguyen N: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (n == i)
+                    {
+                        Console.WriteLine(" {0} la so nguyen to", n);
+                    }
+                    else
+                    {
+                        Console.WriteLine(" {0} Khong phai la so nguyen to", n);
+                    }
+                    break;
+                }
+            }
+        }
+
+        private static void UCLN()
+        {
+            //
+            Console.WriteLine("Nhap a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            int b = int.Parse(Console.ReadLine());
+            while (a*b > 0)
+            {
+                if (a < b)
+                {
+                    b = b % a;
+                }
+                else
+                {
+                    a = a % b;
+                }
+            }
+            Console.WriteLine("UCLN La: {0}", a + b);
+        }
+    }
+}
